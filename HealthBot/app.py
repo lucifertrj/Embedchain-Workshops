4d3d82207a350f7db74740efc131e6bce97f16d8
Tarun Jain <jaintarun.abd17@gmail.com>
2023-10-11 23:06:46 +0530

    update code with comments

--- HealthBot/app.py
+++ HealthBot/app.py
@@ -1,31 +1,38 @@
+# Step-1: Import the required libraries
 import streamlit as st
 from embedchain import App
-import pandas as pd
 from api import getKey
 import os
+import pandas as pd
+# Setup OpenAi api key => Refer documentation and use Open Source App
 
 api_key = getKey()
 os.environ['OPENAI_API_KEY'] = api_key
-app = App()
+app = App() #First line of Embedchain -> LLM Model
 
-st.title("Health Assistant Bot")
+st.title("Heath Assistant Bot")
+
+#In general, we need only 3 line to build a ChatBot-> Embedchain
+
 choice = st.sidebar.selectbox("Pick your choice",("Keto","Vegan"))
 st.header(choice)
 
+#Based on the user choice, we are reading the data
 if choice == 'Keto':
-    file_path = "Data/keto.csv"
+    file_path = "./Data/keto.csv"
     data = pd.read_csv(file_path)
 elif choice == 'Vegan':
-    file_path = "Data/vegan.csv"
+    file_path = "./Data/vegan.csv"
     data = pd.read_csv(file_path)
-else:
-    pass
-    
-st.sidebar.dataframe(data.head(8))
-app.add(file_path,data_type="csv")
 
+#Display
+st.sidebar.dataframe(data.head(10))
+#Need to Add the data to App=> LLM
+app.add(file_path,data_type="csv") #second line of Embedchain
+# Prompt and Query => Response
 prompt = st.text_input("Enter your query")
-if prompt: 
-    with st.spinner("Be healthy..."):
-        response = app.chat(prompt)
+if prompt:
+    with st.spinner("Generating..."):
+        #third line of Embedchain
+        response = app.query(prompt)
         st.write(response)
