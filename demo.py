8d3018708f960eab7063b11a2b5db23fc75791fb
Tarun Jain <jaintarun.abd17@gmail.com>
2023-10-11 23:10:00 +0530

    Oct 11 Workshop- Streamlit demp

--- demo.py
+++ demo.py
@@ -1,43 +1,45 @@
-#Import the library
+# Best practices you need to follow
+
+# Step-1: Create virtual environment - virtualenv env
+# Step-2: Activate: source env/bin/activate
+# Step-3: Install modules: pip install embedchain streamlit
+
+# Start with the live demo
 import streamlit as st
-#Installation: pip install streamlit
-#Running Syntax: streamlit run <filename.py>
-#Running Example: streamlit run demo.py
 
-#Widget=> Building blocks of Streamlit
-#1. Title 
-st.title("Embedchain Workshop")
+#Widgets
+#1. Title
+st.title("Health Assistant Bot")
 #2. Heading
-st.header("IIT Bombay")
-#3. Markdown -> Document language that ends with .md file
-st.markdown("# Heading1")
-st.markdown("## Heading2")
-st.markdown("### Heading3")
-st.markdown("#### Heading4")
-st.markdown("##### Heading5")
-st.markdown("###### Heading6")
-st.markdown("[Embedchain GitHub Repo](https://github.com/embedchain/embedchain/issues)")
+st.header("IIT Ropar Workshop")
+st.sidebar.header("Workshop by Tarun Jain")
 
-#4. Normal paragraph
-st.write("aonwbvsns[pfobfoldcdvonfdvnlmd;   wmm]")
+#3. Normal paragraph or Text
+st.write("Build a ChatBot like ChatGPT on own data")
+#4. Markdown Syntax- Very Important when you are working with Streamlit
+st.markdown("# h1")
+st.markdown("## h2")
+st.markdown("### h3")
+st.markdown("#### h4")
+st.markdown("##### h5")
+st.markdown("###### h6")
+st.markdown("[Embedchain Repo](https://github.com/embedchain/embedchain/)")
+#5. User Input
 
-#5. Sidebar
-st.sidebar.title("Research Paper Bot")
-st.sidebar.markdown("[Source Code](https://github.com/lucifertrj/Embedchain-Workshops/tree/main/ResearchPaperBot)")
+username = st.text_input("Enter your name")
+if username:
+    st.write(username)
+bio = st.text_area("Enter your bio")
+#6. Button/ Selectbox/Radiobutton or Checkbox
 
-#6. User Input
+valid = st.button("Click me") #boolean
+if valid:
+    st.write("Button pressed")
 
-#text input => Small entry box to enter
-#text area => Larger entry box to enter
-user_input = st.text_area("Enter your Name")
-st.write(user_input)
+choice = st.selectbox("Fav CWC2023 team",("India","Aus","Eng","NZ"))
 
-#7. Button
-#return in Boolean
-btn = st.button("Click here")
-if btn:
-    st.markdown("Hi, How are you?")
-    
-# 8. Selectbox
-choice = st.selectbox("CWC23",("Ind vs Pak","Eng vs NZ"))
-    
+#7. File Upload 
+
+uploaded_files = st.file_uploader("Upload your Image")
+
+#streamlit run demo.py
